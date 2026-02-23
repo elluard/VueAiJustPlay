3622601eb35fbe0e852b64eb531a7e8ff867efbe
elluard <elluard@me.com>
2026-02-23 21:51:17 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div>
+      <h1>Vue Ai Just Play</h1>
+
+      <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+    </div>
+  </header>
+  <body>
+    <!-- <RouterView /> -->
+    여기에 뭔가 만들어보려 합니다.
+  </body>
+</template>
+
+<style scoped>
+
+</style>
